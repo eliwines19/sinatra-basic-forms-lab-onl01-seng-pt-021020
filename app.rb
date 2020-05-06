76533f453c51f97260d7f53f05e0c0c7518caba1
Elijah wines <elijahwines84@yahoo.com>
2020-05-06 15:58:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,9 +11,9 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-      @puppy = Puppy.new(params[:name],params[:breed],params[:age])
+    @puppy = Puppy.new(params[:name],params[:breed],params[:age])
 
-      erb :display_puppy
-  end 
+    erb :display_puppy
+  end
 
 end
